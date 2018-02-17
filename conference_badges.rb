8243ea9c3182c1774e3b9f4622c670d461bb1ffb
Allyson Hotchkin <ahotchk1@yahoo.com>
2018-02-17 22:04:46 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -17,6 +17,6 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  batch_badge_creator(attendees)
-  assign_rooms(attendees)
+  print batch_badge_creator(attendees)
+  print assign_rooms(attendees)
 end
